a02acf4f4427ba314ece2d92de8281085341d7c0
SamuraiPolix <sam.lazareanu@gmail.com>
2024-05-17 14:19:38 +0300

    added all operations - need to test

--- Graph.cpp
+++ Graph.cpp
@@ -5,7 +5,6 @@
 */
 
 #include "Graph.hpp"
-#include <iostream>
 
 using namespace std;
 namespace ariel {
@@ -24,29 +23,55 @@ namespace ariel {
         this->cycles = false;
     }
 
-    void Graph::printGraph() {
+    void Graph::printGraph() const {
         size_t vertices = 0;
         size_t edges = 0;
         
-        for (size_t i = 0; i < this->adjMatrix.size(); ++i) {
-            vertices++;     // each row represents a vertice
-            for (size_t j = 0; j < this->adjMatrix[i].size(); ++j) {
-                if (this->adjMatrix[i][j] != 0){        // 0 means there is no edge between vertices i and j
-                    edges++;
-                }
-                cout << this->adjMatrix[i][j] << " ";       // print element
-            }
-            cout << endl;       // line drop
-        }
+        // for (size_t i = 0; i < this->adjMatrix.size(); ++i) {
+        //     vertices++;     // each row represents a vertice
+        //     for (size_t j = 0; j < this->adjMatrix[i].size(); ++j) {
+        //         if (this->adjMatrix[i][j] != 0){        // 0 means there is no edge between vertices i and j
+        //             edges++;
+        //         }
+        //         cout << this->adjMatrix[i][j] << " ";       // print element
+        //     }
+        //     cout << endl;       // line drop
+        // }
+        cout << this->printMatrix();
 
         // Print stats
         cout << "Graph with " << vertices << " vertices and " << edges << " edges." << endl;        
     }
 
-    size_t Graph::getSize(){
+    string Graph::printMatrix() const{
+        string result = "";
+        for (size_t i = 0; i < adjMatrix.size(); ++i) {
+            for (size_t j = 0; j < adjMatrix[i].size(); ++j) {
+                result += adjMatrix[i][j];       // print element
+                result += " ";
+            }
+            result += "\n";       // line drop
+        }
+        return result;
+    }
+
+    size_t Graph::getSize() const{
         return adjMatrix.size();
     }
 
+    size_t Graph::countEdges() const{
+        size_t edges = 0;
+        
+        for (size_t i = 0; i < this->adjMatrix.size(); ++i) {
+            for (size_t j = 0; j < this->adjMatrix[i].size(); ++j) {
+                if (this->adjMatrix[i][j] != 0){        // 0 means there is no edge between vertices i and j
+                    edges++;
+                }
+            }
+        }
+
+        return edges;
+    }
 
     int Graph::getWeight(size_t ver1, size_t ver2) const{
         return this->adjMatrix[ver1][ver2];   
@@ -65,4 +90,258 @@ namespace ariel {
         // If a negative cycles exists - a normal cycles exists as well
         this->cycles = status;
     }
+
+
+    // Helper functions to run operations on an adjMatrix
+    vector<vector<int>> Graph::matrixAdd(const vector<vector<int>>& other) const{
+        // matrix to store the result
+        vector<vector<int>> result(adjMatrix.size(), vector<int>(adjMatrix.size(), 0));
+
+        for(size_t row = 0; row < adjMatrix.size(); row++){
+            for (size_t col = 0; col < adjMatrix[row].size(); col++){
+                result[row][col] = this->adjMatrix[row][col] + other[row][col];
+            }
+        }
+        return result;
+    }
+
+    vector<vector<int>> Graph::matrixAdd(int scalar) const{
+        // matrix to store the result
+        vector<vector<int>> result(adjMatrix.size(), vector<int>(adjMatrix.size(), 0));
+
+        for(size_t row = 0; row < adjMatrix.size(); row++){
+            for (size_t col = 0; col < adjMatrix[row].size(); col++){
+                result[row][col] = this->adjMatrix[row][col] + scalar;
+            }
+        }
+        return result;
+    }
+    vector<vector<int>> Graph::matrixSub(const vector<vector<int>>& other) const{
+        // matrix to store the result
+        vector<vector<int>> result(adjMatrix.size(), vector<int>(adjMatrix.size(), 0));
+
+        for(size_t row = 0; row < adjMatrix.size(); row++){
+            for (size_t col = 0; col < adjMatrix[row].size(); col++){
+                result[row][col] = this->adjMatrix[row][col] - other[row][col];
+            }
+        }
+        return result;
+    }
+
+    vector<vector<int>> Graph::matrixSub(int scalar) const{
+        // matrix to store the result
+        vector<vector<int>> result(adjMatrix.size(), vector<int>(adjMatrix.size(), 0));
+
+        for(size_t row = 0; row < adjMatrix.size(); row++){
+            for (size_t col = 0; col < adjMatrix[row].size(); col++){
+                result[row][col] = this->adjMatrix[row][col] - scalar;
+            }
+        }
+        return result;
+    }
+    vector<vector<int>> Graph::matrixMul(const vector<vector<int>>& other) const{
+        // matrix to store the result
+        vector<vector<int>> result(adjMatrix.size(), vector<int>(adjMatrix.size(), 0));
+
+        for(size_t row = 0; row < adjMatrix.size(); row++){
+            for (size_t col = 0; col < adjMatrix[row].size(); col++){
+                for (size_t ind = 0; ind < adjMatrix.size(); ind++) {
+                    result[row][col] += adjMatrix[row][ind] * adjMatrix[ind][col];
+                }
+            }
+        }
+        return result;
+    }
+
+    vector<vector<int>> Graph::matrixMul(int scalar) const{
+        // matrix to store the result
+        vector<vector<int>> result(adjMatrix.size(), vector<int>(adjMatrix.size(), 0));
+
+        for(size_t row = 0; row < adjMatrix.size(); row++){
+            for (size_t col = 0; col < adjMatrix[row].size(); col++){
+                result[row][col] = this->adjMatrix[row][col] * scalar;
+            }
+        }
+        return result;
+    }
+
+    // Operators
+    // prefix
+    Graph& Graph::operator++(){
+        this->matrixAdd(1);
+        return *this;
+    }
+    
+    Graph& Graph::operator--(){
+        this->matrixSub(1);
+        return *this;
+    }
+
+    // // postfix
+    Graph Graph::operator++(int scalar){
+        Graph temp = *this;     // TODO understand why we do this
+        this->matrixAdd(1);
+        return temp;
+    }
+    Graph Graph::operator--(int){
+        Graph temp = *this;     // TODO understand why we do this
+        this->matrixSub(1);
+        return temp;
+    }
+
+    Graph& Graph::operator+=(const Graph& right){
+        if (this->adjMatrix.size() != right.adjMatrix.size() || this->adjMatrix[0].size() != right.adjMatrix[0].size()){
+            throw invalid_argument("Cannot operate on graphs of different sizes!\n");
+        }
+        this->loadGraph(this->matrixAdd(right.adjMatrix));
+        return *this;
+    }
+    Graph& Graph::operator+=(const int right){
+        this->loadGraph(this->matrixAdd(right));
+        return *this;
+    }
+    Graph& Graph::operator-=(const Graph& right){
+        if (this->adjMatrix.size() != right.adjMatrix.size() || this->adjMatrix[0].size() != right.adjMatrix[0].size()){
+            throw invalid_argument("Cannot operate on graphs of different sizes!\n");
+        }
+        this->loadGraph(this->matrixSub(right.adjMatrix));
+        return *this;
+    }
+    Graph& Graph::operator-=(const int right){
+        this->loadGraph(this->matrixSub(right));
+        return *this;
+    }
+    Graph& Graph::operator*=(const Graph& right){
+        if (this->adjMatrix[0].size() != right.adjMatrix.size()){
+            throw invalid_argument("Cannot operate on graphs with unmatching cols and rows!\n");
+        }
+        this->loadGraph(this->matrixMul(right.adjMatrix));
+        return *this;
+    }
+    Graph& Graph::operator*=(const int right){
+        this->loadGraph(this->matrixMul(right));
+        return *this;
+    }
+    Graph& Graph::operator/=(const int right){
+        this->loadGraph(this->matrixMul(1/right));
+        return *this;
+    }
+
+    // -binary operators-
+    Graph operator+(const Graph& left, const Graph& right){
+        if (left.adjMatrix.size() != right.adjMatrix.size() || left.adjMatrix[0].size() != right.adjMatrix[0].size()){
+            throw invalid_argument("Cannot operate on graphs of different sizes!\n");
+        }
+        return Graph(left.matrixAdd(right.adjMatrix));
+    }
+    Graph operator+(const Graph& left, int value){
+        return Graph(left.matrixAdd(value));
+    }
+    Graph operator+(double value, const Graph& right){
+        return Graph(right.matrixAdd(value));
+    }
+
+    Graph operator-(const Graph& left, const Graph& right){
+        if (left.adjMatrix.size() != right.adjMatrix.size() || left.adjMatrix[0].size() != right.adjMatrix[0].size()){
+            throw invalid_argument("Cannot operate on graphs of different sizes!\n");
+        }
+        return Graph(right.matrixSub(right.adjMatrix));
+    }
+    Graph operator-(const Graph& left, int value){
+        return Graph(left.matrixSub(value));
+    }
+    Graph operator-(double value, const Graph& right){
+        return Graph(right.matrixSub(value));
+    }
+
+    Graph operator*(const Graph& left, const Graph& right){
+        if (left.adjMatrix[0].size() != right.adjMatrix.size()){
+            throw invalid_argument("Cannot operate on graphs with unmatching cols and rows!\n");
+        }
+        return Graph(left.matrixMul(right.adjMatrix));
+    }
+    Graph operator*(const Graph& left, int value){
+        return Graph(left.matrixMul(value));
+    }
+    Graph operator*(double value, const Graph& right){
+        return Graph(right.matrixMul(value));
+    }
+
+    // Graph operator/(const Graph& left, const Graph& right){
+    //     if (left.adjMatrix[0].size() != right.adjMatrix.size()){
+    //         throw invalid_argument("Cannot operate on graphs with unmatching cols and rows!\n");
+    //     }
+    //     return Graph(left.matrixMul(right.adjMatrix));
+    // }
+    Graph operator/(const Graph& left, int value){
+        return Graph(left.matrixMul(1/value));
+    }
+    Graph operator/(double value, const Graph& right){
+        return Graph(right.matrixMul(1/value));
+    }
+
+    // -comparison operators-
+    bool Graph::operator==(const Graph& other) const{
+        return this->adjMatrix == other.adjMatrix || (!(*this > other) && !(other > *this));
+    }
+    bool Graph::operator!=(const Graph& other) const{
+        return !(*this == other);       // using the == operator we implemented
+    }
+
+    bool Graph::operator<(const Graph& other) const{
+        // Graph G2 is larger than graph G1 if graph G1 is contained exactly in graph G2.
+        if (this->isSubgraphOf(other)){
+            return true;
+        }
+        // If neither graph is exactly contained in the other and the graphs are not equal
+        if (!other.isSubgraphOf(*this) && other!=(*this)){       // using our implemented !=
+            size_t this_edges = this->countEdges();
+            size_t other_edges = other.countEdges();
+            // then graph G2 is greater than graph G1 if the number of edges in G2 is greater than the number of edges in G1.
+            if(other_edges > this_edges){   
+                return true;
+            }
+            // If the number of edges is the same,
+            // then the graph G2 is larger than the graph G1 if the representative matrix of G2 has a higher order of magnitude than G1.
+            else if(other_edges == this_edges || other.getSize() > this->getSize()){        // using our implemented ==
+                return true;
+            }
+        }
+        return false;
+    }
+    bool Graph::operator<=(const Graph& other) const{
+        return (*this) < other || (*this) == other;     // using our implemented < , ==
+    }
+    bool Graph::operator>(const Graph& other) const{
+        // Graph G2 is larger than graph G1 if graph G1 is contained exactly in graph G2.
+        if (other.isSubgraphOf(*this)){
+            return true;
+        }
+        // If neither graph is exactly contained in the other and the graphs are not equal
+        if (!other.isSubgraphOf(*this) && other!=(*this)){       // using our implemented !=
+            size_t this_edges = this->countEdges();
+            size_t other_edges = other.countEdges();
+            // then graph G2 is greater than graph G1 if the number of edges in G2 is greater than the number of edges in G1.
+            if(this_edges > other_edges){   
+                return true;
+            }
+            // If the number of edges is the same,
+            // then the graph G2 is larger than the graph G1 if the representative matrix of G2 has a higher order of magnitude than G1.
+            else if(this_edges == other_edges || other.getSize() < this->getSize()){        // using our implemented ==
+                return true;
+            }
+        }
+        return false;
+    }
+    bool Graph::operator>=(const Graph& other) const{
+        return (*this) > other || (*this) == other;     // using our implemented > , ==
+    }
+
+    // -stream operators-
+    ostream& operator<<(ostream& out, const Graph& graph){
+        return (out << graph.printMatrix());
+    }
+    // istream& Graph::operator>>(istream& in, Graph& graph){
+    //     return (out << graph.printMatrix());
+    // }
 }
--- Graph.hpp
+++ Graph.hpp
@@ -6,6 +6,7 @@
 
 #pragma once
 #include <vector>
+#include <iostream>
 
 using namespace std;
 namespace ariel {
@@ -15,6 +16,19 @@ namespace ariel {
         vector<vector<int>> adjMatrix;
         bool negativeCycles;
         bool cycles;
+
+        string printMatrix() const;
+
+        // Helper functions for Graph operations - operations on adjMatrix
+        vector<vector<int>> matrixAdd(const vector<vector<int>>&) const; 
+        vector<vector<int>> matrixAdd(int) const;
+        vector<vector<int>> matrixSub(const vector<vector<int>>&) const; 
+        vector<vector<int>> matrixSub(int) const;
+        vector<vector<int>> matrixMul(const vector<vector<int>>&) const; 
+        vector<vector<int>> matrixMul(int) const;
+
+        // Helper functions for comperison opertaions
+        bool isSubgraphOf(const Graph&) const;
 
         public:
             // Inline Constructors
@@ -33,18 +47,75 @@ namespace ariel {
             /*
              * Prints the adjMatrix and the number of verices and edges in the graph.
             */
-            void printGraph();
+            void printGraph() const;
 
             /*
              * Returns the size of the AdjMatrix of the graph.
              * Size represents the number vertices = number of rows and cols (square matrix).
             */ 
-            size_t getSize();
-
+            size_t getSize() const;
+            size_t countEdges() const;
             int getWeight(size_t, size_t) const;
             bool getCycles() const;
             bool getNegativeCycles() const;
             void setCycles(bool);
             void setNegativeCycles(bool);
+
+            // Operators
+            // prefix
+            Graph& operator++();
+            Graph& operator--();
+            // postfix
+            Graph operator++(int);
+            Graph operator--(int);
+
+            // -compound assignment-
+            Graph& operator+=(const Graph&);
+            Graph& operator+=(const int);
+            Graph& operator-=(const Graph&);
+            Graph& operator-=(const int);
+            Graph& operator*=(const Graph&);
+            Graph& operator*=(const int);
+            Graph& operator/=(const int);
+
+            // -comparison operators-
+            /* Graphs G1 and G2 will be called equal if they are of the same order of magnitude and contain the same edges
+             * (and the weights of the edges are the same)
+             * or if G1 is not greater than G2 and neither is G2 greater than G1.
+             */
+            bool operator==(const Graph& other) const;
+            bool operator!=(const Graph& other) const;
+            /* Graph G2 is larger than graph G1 if graph G1 is contained exactly in graph G2.
+             * If neither graph is exactly contained in the other and the graphs are not equal,
+             * then graph G2 is greater than graph G1 if the number of edges in G2 is greater than the number of edges in G1.
+             * If the number of edges is the same,
+             * then the graph G2 is larger than the graph G1 if the representative matrix of G2 has a higher order of magnitude than G1.
+             */
+            bool operator<(const Graph& other) const;
+            bool operator<=(const Graph& other) const;
+            bool operator>(const Graph& other) const;
+            bool operator>=(const Graph& other) const;
+
+            // -binary operators-
+            // *All set as friend to allow access to private members and methods
+            friend Graph operator+(const Graph&, const Graph&);
+            friend Graph operator+(const Graph&, int);
+            friend Graph operator+(double, const Graph&);
+
+            friend Graph operator-(const Graph&, const Graph&);
+            friend Graph operator-(const Graph&, int);
+            friend Graph operator-(double, const Graph&);
+
+            friend Graph operator*(const Graph&, const Graph&);
+            friend Graph operator*(const Graph&, int);
+            friend Graph operator*(double, const Graph&);
+
+            // friend Graph operator/(const Graph&, const Graph&);
+            friend Graph operator/(const Graph&, int);
+            friend Graph operator/(double, const Graph&);
+
+            // -stream operators-
+            friend ostream& operator<<(ostream&, const Graph&);
+            // friend istream& operator>>(istream&, Graph&);
     };
 }
