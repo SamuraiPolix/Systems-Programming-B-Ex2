1b7e11fbafa8d4ad2926034b19c2804dacef4179
SamuraiPolix <sam.lazareanu@gmail.com>
2024-05-19 13:54:10 +0300

    fix example

--- Demo.cpp
+++ Demo.cpp
@@ -40,8 +40,8 @@ int main()
 
     g1 /= -2;
     ariel::Graph g4 = g1 * g2; // Multiply the two graphs together.
-    cout<<g4;           // Should print the multiplication of the matrices of g1 and g2: [0, 0, 2], [1, 0, 1], [1, 0, 0]
-
+    cout<<g4;           // Should print the multiplication of the matrices of g1 and g2: [1, 0, 2], [1, 3, 1], [1, 0, 2]
+    
     // 5x5 matrix that represents a connected graph.
     vector<vector<int>> graph2 = {
         {0, 1, 0, 0, 1},
